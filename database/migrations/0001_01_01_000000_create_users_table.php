80ad7ef5e63d263df1c9016ca539e272f3cc2f06
Miro Calope Lim <limmiro88@gmail.com>
2026-08-09 18:13:57 +0800

    feat: Remove unused password_reset_tokens table and related migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -40,12 +40,6 @@ return new class extends Migration
             $table->index(['role_id', 'is_active']);
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
@@ -62,7 +56,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
